cfcfea53f52eab2ab7ff134e154bb85d472ebca8
selincs <selin.benan7@gmail.com>
2024-11-08 18:08:55 -0500

    Created new branch and transferred previous work to firebase-work branch
    Refactored project structure to include controller + model packages for files
    
    Changed name of Controller to match its function, controller itself may be temporary as login function must be figured out through Firebase first

--- src/main/java/csc325/collectionsproject/CollectionsApplication.java
+++ src/main/java/csc325/collectionsproject/CollectionsApplication.java
@@ -1,27 +1,45 @@
 package csc325.collectionsproject;
 
+import csc325.collectionsproject.model.FirestoreContext;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.cloud.FirestoreClient;
 
+import com.google.firebase.auth.*;
+import com.google.cloud.firestore.*;
+import com.google.api.core.ApiFuture;
 import java.io.IOException;
 
 public class CollectionsApplication extends Application {
-    public static Scene scene;
+    public static Scene scene;    // Firestore reference
+    public static Firestore fstoreDB;
+    public static FirebaseAuth fauth;
+    private final FirestoreContext contxtFirebase = new FirestoreContext();
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CollectionsApplication.class.getResource("collection-view.fxml"));
+        //Initialize Firestore, authorization, db
+        fstoreDB = contxtFirebase.firebase();
+        fauth = FirebaseAuth.getInstance();
+
+ //     FXMLLoader fxmlLoader = new FXMLLoader(CollectionsApplication.class.getResource("collectionview.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CollectionsApplication.class.getResource("registration-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 860, 640);
         stage.setTitle("CollectionsApp");
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
@@ -33,4 +51,6 @@ public class CollectionsApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,17 @@
 module csc325.collectionsproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires firebase.admin;
+    requires com.google.auth;
+    requires com.google.auth.oauth2;
+    requires google.cloud.firestore;
+    requires google.cloud.core;
+    requires com.google.api.apicommon;
 
     opens csc325.collectionsproject to javafx.fxml;
     exports csc325.collectionsproject;
+    exports csc325.collectionsproject.controller;
+    opens csc325.collectionsproject.controller to javafx.fxml;
+    exports csc325.collectionsproject.model;
+    opens csc325.collectionsproject.model to javafx.fxml;
 }
