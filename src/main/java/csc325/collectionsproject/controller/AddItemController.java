85a0b23ab3c933201b55173df2ae72645a692b4a
selincs <selin.benan7@gmail.com>
2024-11-08 22:40:12 -0500

    Created add-item-view GUI for a Collection (Crying in the club over github)

---
+++ src/main/java/csc325/collectionsproject/controller/AddItemController.java
@@ -0,0 +1,20 @@
+package csc325.collectionsproject.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class AddItemController {
+
+        @FXML
+        private Button addImgBtn;
+
+        @FXML
+        private ImageView addItemImg;
+
+        @FXML
+        private HBox privateToggle;
+
+
+}
