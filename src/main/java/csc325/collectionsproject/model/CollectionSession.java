4a8196906f239f5f05ecf4c43378b4744d601a21
Serena <davam@farmingdale.edu>
2024-12-07 21:31:11 -0500

    collections descriptions working -- item view is next

--- src/main/java/csc325/collectionsproject/model/CollectionSession.java
+++ src/main/java/csc325/collectionsproject/model/CollectionSession.java
@@ -6,6 +6,7 @@ public class CollectionSession {
         // Variable to store the logged-in user's information
         private String selectedCollectionName;
         private String selectedCollectionItemName;
+         private String selectedCollectionDescription;
 
         // Private constructor (to prevent instantiation before registration/login events occur)
         private CollectionSession() {}
@@ -25,6 +26,11 @@ public class CollectionSession {
     public String getSelectedCollectionName() {
         return selectedCollectionName;
     }
+
+    public void setSelectedCollectionDescription(String descriptionText) { this.selectedCollectionDescription = descriptionText; }
+
+
+    public String getSelectedCollectionDescription() { return selectedCollectionDescription;}
 
     public void setSelectedCollectionItemName(String selectedCollectionItemName) {
         this.selectedCollectionItemName = selectedCollectionItemName;
